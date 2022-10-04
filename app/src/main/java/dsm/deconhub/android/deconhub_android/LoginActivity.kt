f7fcfeaac5f9446cc325d4bbe723933c4060e368
이준범 <ljb050310@naver.com>
2022-10-04 16:19:03 +0900

    ⚒️:: 텍스트 버튼 수정

--- app/src/main/java/dsm/deconhub/android/deconhub_android/LoginActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/LoginActivity.kt
@@ -1,12 +1,26 @@
 package dsm.deconhub.android.deconhub_android
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val signButton = findViewById<TextView>(R.id.textButton1)
+        val findPasswordButton = findViewById<TextView>(R.id.textButton2)
+
+        signButton.setOnClickListener {
+            val sign = Intent(this, SignActivity::class.java)
+
+            startActivity(sign)
+        }
+        findPasswordButton.setOnClickListener {
+            val findPassword = Intent(this, FindPasswordActivity::class.java)
+
+            startActivity(findPassword)
+        }
     }
 }
