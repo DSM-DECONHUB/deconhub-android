2ea0b179e0a5368c7c491558d7546a88f28d0885
이준범 <ljb050310@naver.com>
2022-10-05 20:12:45 +0900

    ✖️:: 매인 엑티비티 import 추가

--- app/src/main/java/dsm/deconhub/android/deconhub_android/MainActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import dsm.deconhub.android.deconhub_android.loginsign.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
