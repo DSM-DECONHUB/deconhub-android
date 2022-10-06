5f70a3486b4c773338d94c9f7cfce7668788e06b
이준범 <ljb050310@naver.com>
2022-10-06 10:58:02 +0900

    ✖️:: 로그인 버튼 이동 추가

--- app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/LoginActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/LoginActivity.kt
@@ -2,8 +2,10 @@ package dsm.deconhub.android.deconhub_android.loginsign
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import dsm.deconhub.android.deconhub_android.MainActivity
 import dsm.deconhub.android.deconhub_android.R
 
 class LoginActivity : AppCompatActivity() {
@@ -11,17 +13,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val signButton = findViewById<TextView>(R.id.textButton1)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val login = Intent(this, MainActivity::class.java)
+            login.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(login)
+        }
+
+        val signButton = findViewById<TextView>(R.id.signButton)
         signButton.setOnClickListener {
             val sign = Intent(this, SignActivity::class.java)
-
             startActivity(sign)
         }
 
-        val findPasswordButton = findViewById<TextView>(R.id.textButton2)
+        val findPasswordButton = findViewById<TextView>(R.id.findPasswordButton)
         findPasswordButton.setOnClickListener {
             val findPassword = Intent(this, FindPasswordActivity::class.java)
-
             startActivity(findPassword)
         }
     }
