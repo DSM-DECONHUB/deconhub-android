81e34b9fe3379fcb02698faed4a857b7e8dec969
이준범 <ljb050310@naver.com>
2022-10-06 10:57:28 +0900

    ✖️:: 비밀번호 찾기 버튼 이동 추가

--- app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/FindPasswordActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/FindPasswordActivity.kt
@@ -1,6 +1,8 @@
 package dsm.deconhub.android.deconhub_android.loginsign
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import dsm.deconhub.android.deconhub_android.R
@@ -13,6 +15,12 @@ class FindPasswordActivity : AppCompatActivity() {
         val backButton = findViewById<ImageButton>(R.id.backButton)
         backButton.setOnClickListener {
             onBackPressed()
+        }
+
+        val authenticationNumberButton = findViewById<Button>(R.id.authenticationNumberButton)
+        authenticationNumberButton.setOnClickListener {
+            val authenticationNumber = Intent(this, AuthenticationNumberActivity::class.java)
+            startActivity(authenticationNumber)
         }
     }
 }
