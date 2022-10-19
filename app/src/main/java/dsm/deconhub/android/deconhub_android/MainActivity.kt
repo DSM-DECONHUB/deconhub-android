6784bd1924cf934a6d02236a9d12a13f6f6eef32
이준범 <ljb050310@naver.com>
2022-10-19 11:55:15 +0900

    ➕:: 바텀 네비게이션 바 연결

--- app/src/main/java/dsm/deconhub/android/deconhub_android/MainActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/MainActivity.kt
@@ -1,16 +1,62 @@
 package dsm.deconhub.android.deconhub_android
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import dsm.deconhub.android.deconhub_android.loginsign.LoginActivity
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dsm.deconhub.android.deconhub_android.chat.ChatPageFragment
+import dsm.deconhub.android.deconhub_android.home.HomePageFragment
+import dsm.deconhub.android.deconhub_android.mypage.MyPageFragment
+import dsm.deconhub.android.deconhub_android.study.StudyPageFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var bottomNavigationView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        //첫 프래그먼트 화면은 home fragment로
+        bottomNavigationView.selectedItemId = R.id.homeFragment
     }
 
-    val peopleList: List<String> = listOf()
+    //Implement member
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.homeFragment ->{
+                val fragment = HomePageFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.bottom_container,fragment)
+                    // view_main이 보여지는 화면에 fragment를 보여줘라
+                    .commit()
+                return true
+            }
+            R.id.studyFragment ->{
+                val fragment = StudyPageFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.bottom_container,fragment)
+                    .commit()
+                return true
+            }
+            R.id.chatFragment ->{
+                val fragment = ChatPageFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.bottom_container,fragment)
+                    .commit()
+                return true
+            }
+            R.id.myPageFragment ->{
+                val fragment = MyPageFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.bottom_container,fragment)
+                    .commit()
+                return true
+            }
+        }
+        return false
+    }
 }
