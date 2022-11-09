20a90dc676c3418b2a91ffcb454db4f1bfc98e0e
이준범 <ljb050310@naver.com>
2022-11-09 19:35:04 +0900

    🔁:: HomePage Fragment 변경

--- app/src/main/java/dsm/deconhub/android/deconhub_android/home/HomePageFragment.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/home/HomePageFragment.kt
@@ -1,22 +1,38 @@
 package dsm.deconhub.android.deconhub_android.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import dsm.deconhub.android.deconhub_android.R
+import dsm.deconhub.android.deconhub_android.chat.ChatDetailActivity
 import dsm.deconhub.android.deconhub_android.databinding.FragmentHomePageBinding
 
 class HomePageFragment : Fragment() {
-    lateinit var binding: FragmentHomePageBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomePageBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.fragment_home_page, null)
+        val searchButton = view.findViewById<ImageButton>(R.id.searchButton)
+        val notificationButton = view.findViewById<ImageButton>(R.id.notificationButton)
 
-        return binding.root
+        searchButton.setOnClickListener(
+            ({
+            })
+        )
+
+        notificationButton.setOnClickListener(
+            ({
+            })
+        )
+
+        return view
     }
 }
