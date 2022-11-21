2b0d75321474f5112b46247644db26d5e44b1790
이준범 <ljb050310@naver.com>
2022-11-21 09:33:29 +0900

    ➕:: 로그인 api 연결

--- app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/LoginActivity.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/loginsign/LoginActivity.kt
@@ -1,60 +1,57 @@
 package dsm.deconhub.android.deconhub_android.loginsign
 
-import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import android.icu.number.Scale.none
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import dsm.deconhub.android.deconhub_android.MainActivity
 import dsm.deconhub.android.deconhub_android.R
-import dsm.deconhub.android.deconhub_android.data.Login
-import dsm.deconhub.android.deconhub_android.data.LoginService
+import dsm.deconhub.android.deconhub_android.data.RequestLogin
+import dsm.deconhub.android.deconhub_android.data.RequestToServer
+import dsm.deconhub.android.deconhub_android.data.ResponseLogin
 import kotlinx.android.synthetic.main.activity_login.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class LoginActivity : AppCompatActivity() {
-    var login: Login? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var retrofit = Retrofit.Builder()
-            .baseUrl("http://ec2-54-180-109-55.ap-northeast-2.compute.amazonaws.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        var loginService: LoginService = retrofit.create(LoginService::class.java)
+        var id = accountIdLogin.text.toString()
+        var pw = passwordLogin.text.toString()
+
+        val requestToServer = RequestToServer
 
         loginButton.setOnClickListener {
-            var id = accountId.text.toString()
-            var pw = password.text.toString()
-
-            loginService.requestLogin(id, pw).enqueue(object : Callback<Login> {
-                override fun onFailure(call: Call<Login>, t: Throwable) {
-                    Log.e("LOGIN", t.message.toString())
-                    var dialog = AlertDialog.Builder(this@LoginActivity)
-                    dialog.setTitle("에러")
-                    dialog.setMessage("호출실패했습니다.")
-                    dialog.show()
+            Log.d("id, pw", "id $id, pw $pw")
+            requestToServer.service.requestLogin(
+                RequestLogin(accountIdLogin.text.toString(), passwordLogin.text.toString())
+            ).enqueue(object : Callback<ResponseLogin> {
+                override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
+                    Log.d("통신 실패", "${t.message}")
                 }
 
-                override fun onResponse(call: Call<Login>, response: Response<Login>) {
-                    login = response.body()
-                    Log.d("LOGIN", "msg : " + login?.accountId)
-                    Log.d("LOGIN", "code : " + login?.password)
-                    var dialog = AlertDialog.Builder(this@LoginActivity)
-                    dialog.setTitle(login?.accountId)
-                    dialog.setMessage(login?.password)
-                    dialog.show()
+                override fun onResponse(
+                    call: Call<ResponseLogin>,
+                    response: Response<ResponseLogin>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d("성공", response.body()!!.data.toString())
+                        startActivity(Intent(this@LoginActivity, MainActivity::class.java));
+                        finish()
+
+                    } else {
+                        Log.d("실패", "실패")
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "아이디와 비밀번호를 확인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             })
         }
@@ -68,5 +65,7 @@ class LoginActivity : AppCompatActivity() {
             val findPassword = Intent(this, FindPasswordActivity::class.java)
             startActivity(findPassword)
         }
+
+
     }
 }
