8e10cddb1a55fb664c67d71667517f4240875115
이준범 <ljb050310@naver.com>
2022-11-21 09:37:47 +0900

    ➕:: api build 생성

--- app/src/main/java/dsm/deconhub/android/deconhub_android/data/RequestToServer.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/data/RequestToServer.kt
@@ -2,13 +2,13 @@ package dsm.deconhub.android.deconhub_android.data
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
 
-object ServerApi {
+object RequestToServer {
     var retrofit = Retrofit.Builder()
         .baseUrl("http://54.180.109.55/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+    var service: RequestInterface = retrofit.create(
+        RequestInterface::class.java)
 }
