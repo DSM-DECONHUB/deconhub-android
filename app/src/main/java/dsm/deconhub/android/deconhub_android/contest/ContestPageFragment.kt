ceab7e0e45caac593bedf52f7ed8390cc292d9d3
이준범 <ljb050310@naver.com>
2022-12-06 15:26:48 +0900

    ⚒️:: setOnClickListener 수정

--- app/src/main/java/dsm/deconhub/android/deconhub_android/contest/ContestPageFragment.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/contest/ContestPageFragment.kt
@@ -1,11 +1,14 @@
 package dsm.deconhub.android.deconhub_android.contest
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dsm.deconhub.android.deconhub_android.R
 
 class ContestPageFragment : Fragment() {
@@ -16,19 +19,32 @@ class ContestPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_contest_page, null)
-        val searchButton = view.findViewById<ImageButton>(R.id.searchButton)
-        val notificationButton = view.findViewById<ImageButton>(R.id.notificationButton)
+        val searchButton = view.findViewById<ImageButton>(R.id.searchButtonContest)
+        val idContest = view.findViewById<LinearLayout>(R.id.idContest)
+        val createContest = view.findViewById<FloatingActionButton>(R.id.createContestButton)
 
-        searchButton.setOnClickListener(
+        searchButton.setOnClickListener(({
+            val intent = Intent(context, ContestSearchActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }))
+
+
+        idContest.setOnClickListener(
             ({
+                val intent = Intent(context, ContestDetailActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
             })
         )
 
-        notificationButton.setOnClickListener(
+        createContest.setOnClickListener(
             ({
+                val intent = Intent(context, ContestCreateActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
             })
         )
-
         return view
     }
 }
