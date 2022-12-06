c9abfa13b9450ba53ad620663dbf470ac729bd0e
이준범 <ljb050310@naver.com>
2022-12-06 15:27:23 +0900

    ➕:: Retrofit POST, GET 추가

--- app/src/main/java/dsm/deconhub/android/deconhub_android/data/RequestInterface.kt
+++ app/src/main/java/dsm/deconhub/android/deconhub_android/data/RequestInterface.kt
@@ -2,9 +2,21 @@ package dsm.deconhub.android.deconhub_android.data
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface RequestInterface {
     @POST("user/login")
     fun requestLogin(@Body body: RequestLogin): Call<ResponseLogin>
+
+    @POST("uesr/signup")
+    fun requestSignup(@Body body: RequestSignup)
+
+    @GET("contest/list")
+    fun requestContestList(): Call<ResponseContestList>
+
+    @GET("uesr")
+    fun requestUser(@Header("accessToken") accessToken: String): Call<ResponseUser>
 }
